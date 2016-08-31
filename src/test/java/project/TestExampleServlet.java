13364f89ef92d0c1ba276d652c0cc6b277f85e85
yurinahoshi <s1210017@u-aizu.ac.jp>
2016-08-31 18:59:01 +0900

    Create init project.

---
+++ src/test/java/project/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package project;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link project.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
